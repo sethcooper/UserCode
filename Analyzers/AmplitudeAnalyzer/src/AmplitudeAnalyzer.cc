b8f57d35ac036819b50a670a88a9cf5c3a2c5270
scooper <>
2007-08-22 18:54:13 +0000

    version that does something.

--- Analyzers/AmplitudeAnalyzer/src/AmplitudeAnalyzer.cc
+++ Analyzers/AmplitudeAnalyzer/src/AmplitudeAnalyzer.cc
@@ -13,13 +13,15 @@
 //
 // Original Author:  Seth COOPER ()
 //         Created:  Wed Aug 22 18:57:08 CEST 2007
-// $Id$
+// $Id: AmplitudeAnalyzer.cc,v 1.1 2007/08/22 17:11:06 scooper Exp $
 //
 //
 
 
 // system include files
 #include <memory>
+#include <string>
+#include <vector>
 
 // user include files
 #include "FWCore/Framework/interface/Frameworkfwd.h"
@@ -29,6 +31,17 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "DataFormats/EcalDetId/interface/EBDetId.h"
+#include "DataFormats/EcalDigi/interface/EBDataFrame.h"
+#include "DataFormats/EcalDigi/interface/EcalDigiCollections.h"
+#include "DataFormats/EcalRecHit/interface/EcalUncalibratedRecHit.h"
+#include "DataFormats/EcalRecHit/interface/EcalRecHitCollections.h"
+#include "TH1F.h"
+#include "TH2F.h"
+#include "TFile.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "DataFormats/Common/interface/Handle.h"
+
 //
 // class decleration
 //
@@ -44,8 +57,16 @@ class AmplitudeAnalyzer : public edm::EDAnalyzer {
       virtual void analyze(const edm::Event&, const edm::EventSetup&);
       virtual void endJob() ;
 
+      edm::InputTag EBDigiCollection_;
+      edm::InputTag  EcalRecHitCollection_;
+      edm::InputTag EcalUncalibratedRecHitCollection_;
+
       // ----------member data ---------------------------
 };
+
+using namespace std;
+using namespace edm;
+using namespace cms;
 
 //
 // constants, enums and typedefs
@@ -58,10 +79,12 @@ class AmplitudeAnalyzer : public edm::EDAnalyzer {
 //
 // constructors and destructor
 //
-AmplitudeAnalyzer::AmplitudeAnalyzer(const edm::ParameterSet& iConfig)
+AmplitudeAnalyzer::AmplitudeAnalyzer(const ParameterSet& ps)
 {
    //now do what ever initialization is needed
 
+  EBDigiCollection_ = ps.getParameter<InputTag>("EBDigiCollection");
+  EcalUncalibratedRecHitCollection_ = ps.getParameter<InputTag>("EcalUncalibratedRecHitCollection");
 }
 
 
@@ -79,26 +102,41 @@ AmplitudeAnalyzer::~AmplitudeAnalyzer()
 //
 
 // ------------ method called to for each event  ------------
-void
-AmplitudeAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
+void AmplitudeAnalyzer::analyze(const Event& e, const EventSetup& iSetup)
 {
-   using namespace edm;
+  Handle<EBDigiCollection> digis;
+  e.getByLabel(EBDigiCollection_, digis);
+
+  for ( EBDigiCollection::const_iterator digiItr = digis->begin(); digiItr != digis->end(); ++digiItr ) {
+
+    EBDataFrame dataframe = (*digiItr);
+    EBDetId id = dataframe.id();
   
-#ifdef THIS_IS_AN_EVENT_EXAMPLE
-   Handle<ExampleData> pIn;
-   iEvent.getByLabel("example",pIn);
-#endif
-   
-#ifdef THIS_IS_AN_EVENTSETUP_EXAMPLE
-   ESHandle<SetupData> pSetup;
-   iSetup.get<SetupRecord>().get(pSetup);
-#endif
+    for (int i = 0; i < 10; i++) {
+      EcalMGPASample sample = dataframe.sample(i);
+      int adc = sample.adc();
+      int gainid = sample.gainId();
+    }
+  }
+  
+  Handle<EcalUncalibratedRecHitCollection> hits;
+  e.getByLabel(EcalUncalibratedRecHitCollection_, hits);
+  for ( EcalUncalibratedRecHitCollection::const_iterator hitItr = hits->begin(); hitItr != hits->end(); ++hitItr ) {
+
+    EcalUncalibratedRecHit hit = (*hitItr);
+    EBDetId id = hit.id();
+    float R_ene = hit.amplitude();
+    float chi2    = hit.chi2();
+    float jiitter   = hit.jitter();
+cout << "amp: " << R_ene << endl;
+    //fill here
+  }
+
 }
 
 
 // ------------ method called once each job just before starting event loop  ------------
-void 
-AmplitudeAnalyzer::beginJob(const edm::EventSetup&)
+void AmplitudeAnalyzer::beginJob(const EventSetup&)
 {
 }
 
