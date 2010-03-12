93723f3c8db5f5a97a8176c407c7b75c9062746d
scooper <>
2010-03-12 09:36:39 +0000

    Added more plots, timing amplitude cut.

--- Analyzers/SimpleTimeEnergyGraphs/src/SimpleTimeEnergyGraphs.cc
+++ Analyzers/SimpleTimeEnergyGraphs/src/SimpleTimeEnergyGraphs.cc
@@ -13,13 +13,14 @@
 //
 // Original Author:  
 //         Created:  Tue Oct 13 14:56:06 CEST 2009
-// $Id: SimpleTimeEnergyGraphs.cc,v 1.1 2010/03/10 08:19:27 scooper Exp $
+// $Id: SimpleTimeEnergyGraphs.cc,v 1.2 2010/03/10 12:28:27 scooper Exp $
 //
 //
 
 
 // system include files
 #include <memory>
+#include <string>
 
 // user include files
 #include "FWCore/Framework/interface/Frameworkfwd.h"
@@ -29,6 +30,8 @@
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 
 #include "DataFormats/EcalRecHit/interface/EcalRecHitCollections.h"
+#include "DataFormats/EcalDetId/interface/EBDetId.h"
+#include "DataFormats/EcalDetId/interface/EEDetId.h"
 
 #include "TH1.h"
 #include "TH2.h"
@@ -49,18 +52,31 @@ class SimpleTimeEnergyGraphs : public edm::EDAnalyzer {
       virtual void analyze(const edm::Event&, const edm::EventSetup&);
       virtual void endJob() ;
 
+      std::string intToString(int num);
+
       // ----------member data ---------------------------
       edm::InputTag EBRecHitCollection_;
       edm::InputTag EERecHitCollection_;
       edm::InputTag EBUncalibRecHitCollection_;
       edm::InputTag EEUncalibRecHitCollection_;
 
+      double minAmpForTimingEB_;
+      double minAmpForTimingEE_;
+
       TFile* file_;
       TH1F* timeOfHitsEBps_;
       TH1F* timeOfHitsEB_;
+      TH1F* timeOfHitsEB5GeV_;
+      TH1F* timeOfHitsEB10GeV_;
       TH1F* energyOfHitsEB_;
       TH1F* timeOfHitsEE_;
+      TH1F* timeOfHitsEE5GeV_;
+      TH1F* timeOfHitsEE10GeV_;
       TH1F* energyOfHitsEE_;
+
+      TH1F* timeInEtaBinsEB_[6];
+      TH1F* timeInEtaBinsEEP_[4];
+      TH1F* timeInEtaBinsEEM_[4];
 
       TH2F* timeVsAmplitudeEB_;
       TH2F* timeVsAmplitudeEE_;
@@ -81,7 +97,9 @@ SimpleTimeEnergyGraphs::SimpleTimeEnergyGraphs(const edm::ParameterSet& iConfig)
   EBRecHitCollection_ (iConfig.getParameter<edm::InputTag>("EBRecHitCollection")),
   EERecHitCollection_ (iConfig.getParameter<edm::InputTag>("EERecHitCollection")),
   EBUncalibRecHitCollection_ (iConfig.getParameter<edm::InputTag>("EBUncalibRecHitCollection")),
-  EEUncalibRecHitCollection_ (iConfig.getParameter<edm::InputTag>("EEUncalibRecHitCollection"))
+  EEUncalibRecHitCollection_ (iConfig.getParameter<edm::InputTag>("EEUncalibRecHitCollection")),
+  minAmpForTimingEB_ (iConfig.getUntrackedParameter<double>("minAmpForTimingEB",5)),
+  minAmpForTimingEE_ (iConfig.getUntrackedParameter<double>("minAmpForTimingEE",5))
 {
    //now do what ever initialization is needed
    file_ = new TFile("simpleTimeEnergyGraphs.root","RECREATE");
@@ -89,12 +107,33 @@ SimpleTimeEnergyGraphs::SimpleTimeEnergyGraphs(const edm::ParameterSet& iConfig)
 
    timeOfHitsEBps_ = new TH1F("timeOfHitsEBps", "Time of hits EB [ps]",2000,-100,100);
    timeOfHitsEB_ = new TH1F("timeOfHitsEB", "Time of hits EB [ns]",10000,-5,5);
+   timeOfHitsEB5GeV_ = new TH1F("timeOfHitsEB5GeV", "Time of hits EB > 5 GeV [ns]",10000,-5,5);
+   timeOfHitsEB10GeV_ = new TH1F("timeOfHitsEB10GeV", "Time of hits EB > 10 GeV [ns]",10000,-5,5);
    energyOfHitsEB_ = new TH1F("energyOfHitsEB", "Energy of hits EB [GeV]",2500,0,100);
    timeOfHitsEE_ = new TH1F("timeOfHitsEE", "Time of hits EE [ns]",10000,-5,5);
+   timeOfHitsEE5GeV_ = new TH1F("timeOfHitsEE5GeV", "Time of hits EE > 5 GeV [ns]",10000,-5,5);
+   timeOfHitsEE10GeV_ = new TH1F("timeOfHitsEE10GeV", "Time of hits EE > 10 GeV [ns]",10000,-5,5);
    energyOfHitsEE_ = new TH1F("energyOfHitsEE", "Energy of hits EE [GeV]",2500,0,100);
 
    timeVsAmplitudeEB_ = new TH2F("timeVsAmplitudeEB","Time vs. amplitude of hits EB;ADC;ns",1250,0,2500,1000,-25,25);
    timeVsAmplitudeEE_ = new TH2F("timeVsAmplitudeEE","Time vs. amplitude of hits EE;ADC;ns",1250,0,2500,1000,-25,25);
+
+   for(int i=0; i<6; ++i)
+   {
+     std::string histTitle="timeOfHitsEBetaBin";
+     histTitle+=intToString(i);
+     timeInEtaBinsEB_[i] = new TH1F(histTitle.c_str(),histTitle.c_str(),1000,-5,5);
+   }
+   // EE
+   for(int i=0; i<4; ++i)
+   {
+     std::string histTitle="timeOfHitsEEMetaBin";
+     histTitle+=intToString(i);
+     timeInEtaBinsEEM_[i] = new TH1F(histTitle.c_str(),histTitle.c_str(),1000,-5,5);
+     std::string histTitle1="timeOfHitsEEPetaBin";
+     histTitle1+=intToString(i);
+     timeInEtaBinsEEP_[i] = new TH1F(histTitle1.c_str(),histTitle1.c_str(),1000,-5,5);
+   }
 }
 
 
@@ -160,9 +199,6 @@ SimpleTimeEnergyGraphs::analyze(const edm::Event& iEvent, const edm::EventSetup&
   for(EBRecHitCollection::const_iterator recHitItr = ebRecHits->begin();
       recHitItr != ebRecHits->end(); ++recHitItr)
   {
-    timeOfHitsEBps_->Fill(recHitItr->time()*1000);
-    timeOfHitsEB_->Fill(recHitItr->time());
-    energyOfHitsEB_->Fill(recHitItr->energy());
     // Find UncalibHit
     EcalUncalibratedRecHitCollection::const_iterator uHitItr = ebURecHits->begin();
     while(uHitItr != ebURecHits->end() && uHitItr->id() != recHitItr->id())
@@ -171,16 +207,39 @@ SimpleTimeEnergyGraphs::analyze(const edm::Event& iEvent, const edm::EventSetup&
       timeVsAmplitudeEB_->Fill(uHitItr->amplitude(),recHitItr->time());
     else
       continue;
-    std::cout << "Time of EBRecHit: " << recHitItr->time() << " ns, energy: "
-      << recHitItr->energy() << " GeV, amplitude: " << uHitItr->amplitude()
-      << std::endl;
+    if(uHitItr->amplitude() < minAmpForTimingEB_)
+      continue;
+
+    timeOfHitsEBps_->Fill(recHitItr->time()*1000);
+    timeOfHitsEB_->Fill(recHitItr->time());
+    energyOfHitsEB_->Fill(recHitItr->energy());
+    if(recHitItr->energy() > 5)
+      timeOfHitsEB5GeV_->Fill(recHitItr->time());
+    if(recHitItr->energy() > 10)
+      timeOfHitsEB10GeV_->Fill(recHitItr->time());
+    EBDetId id = ((EBDetId)recHitItr->id());
+    // Eta bins
+    if(id.ieta() > 57 && id.ieta() < 86)
+      timeInEtaBinsEB_[5]->Fill(recHitItr->time());
+    if(id.ieta() > 29 && id.ieta() < 58)
+      timeInEtaBinsEB_[4]->Fill(recHitItr->time());
+    if(id.ieta() > 0 && id.ieta() < 30)
+      timeInEtaBinsEB_[3]->Fill(recHitItr->time());
+    if(id.ieta() > -30 && id.ieta() < 0)
+      timeInEtaBinsEB_[2]->Fill(recHitItr->time());
+    if(id.ieta() > -58 && id.ieta() < -29)
+      timeInEtaBinsEB_[1]->Fill(recHitItr->time());
+    if(id.ieta() > -86 && id.ieta() < -57)
+      timeInEtaBinsEB_[0]->Fill(recHitItr->time());
+
+    //std::cout << "Time of EBRecHit: " << recHitItr->time() << " ns, energy: "
+    //  << recHitItr->energy() << " GeV, amplitude: " << uHitItr->amplitude()
+    //  << std::endl;
   }
   // Loop over EERecHits
   for(EERecHitCollection::const_iterator recHitItr = eeRecHits->begin();
       recHitItr != eeRecHits->end(); ++recHitItr)
   {
-    timeOfHitsEE_->Fill(recHitItr->time());
-    energyOfHitsEE_->Fill(recHitItr->energy());
     // Find UncalibHit
     EcalUncalibratedRecHitCollection::const_iterator uHitItr = eeURecHits->begin();
     while(uHitItr != eeURecHits->end() && uHitItr->id() != recHitItr->id())
@@ -189,9 +248,19 @@ SimpleTimeEnergyGraphs::analyze(const edm::Event& iEvent, const edm::EventSetup&
       timeVsAmplitudeEE_->Fill(uHitItr->amplitude(),recHitItr->time());
     else
       continue;
-    std::cout << "Time of EERecHit: " << recHitItr->time() << " ns, energy: "
-      << recHitItr->energy() << " GeV, amplitude: " << uHitItr->amplitude()
-      << std::endl;
+    if(uHitItr->amplitude() < minAmpForTimingEE_)
+      continue;
+
+    timeOfHitsEE_->Fill(recHitItr->time());
+    energyOfHitsEE_->Fill(recHitItr->energy());
+    if(recHitItr->energy() > 5)
+      timeOfHitsEE5GeV_->Fill(recHitItr->time());
+    if(recHitItr->energy() > 10)
+      timeOfHitsEE10GeV_->Fill(recHitItr->time());
+
+    //std::cout << "Time of EERecHit: " << recHitItr->time() << " ns, energy: "
+    //  << recHitItr->energy() << " GeV, amplitude: " << uHitItr->amplitude()
+    //  << std::endl;
   }
 
 }
@@ -210,14 +279,36 @@ SimpleTimeEnergyGraphs::endJob() {
 
   timeOfHitsEBps_->Write();
   timeOfHitsEB_->Write();
+  timeOfHitsEB5GeV_->Write();
+  timeOfHitsEB10GeV_->Write();
   energyOfHitsEB_->Write();
   timeVsAmplitudeEB_->Write();
   timeOfHitsEE_->Write();
+  timeOfHitsEE5GeV_->Write();
+  timeOfHitsEE10GeV_->Write();
   energyOfHitsEE_->Write();
   timeVsAmplitudeEE_->Write();
 
+  // EB
+  for(int i=0; i<6; ++i)
+    timeInEtaBinsEB_[i]->Write();
+  // EE
+  //for(int i=0; i<4; ++i)
+  //{
+  //  timeInEtaBinsEEM_[i]->Write();
+  //  timeInEtaBinsEEP_[i]->Write();
+  //}
+
   file_->Close();
-  
+
+}
+
+std::string SimpleTimeEnergyGraphs::intToString(int num)
+{
+  using namespace std;
+  ostringstream myStream;
+  myStream << num << flush;
+  return(myStream.str()); //returns the string form of the stringstream object
 }
 
 //define this as a plug-in
