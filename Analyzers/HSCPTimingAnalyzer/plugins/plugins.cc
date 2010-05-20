5e53819604e22cddb87eb443ac0e76d8dae6e51a
scooper <>
2010-05-20 14:38:15 +0000

    Moving analyzer to plugin structure.

---
+++ Analyzers/HSCPTimingAnalyzer/plugins/plugins.cc
@@ -0,0 +1,5 @@
+#include <FWCore/Framework/interface/MakerMacros.h>
+#include "Analyzers/HSCPTimingAnalyzer/plugins/HSCPTimingAnalyzer.h"
+
+//define this as a plug-in
+DEFINE_FWK_MODULE(HSCPTimingAnalyzer);
