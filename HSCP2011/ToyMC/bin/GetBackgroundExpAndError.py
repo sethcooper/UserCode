5d5fc0fd9c2a9530220639a80cbb0d2804bf5195
scooper <>
2012-02-21 22:41:29 +0000

    Add rounding, sigEff.

--- HSCP2011/ToyMC/bin/GetBackgroundExpAndError.py
+++ HSCP2011/ToyMC/bin/GetBackgroundExpAndError.py
@@ -25,32 +25,54 @@ def GetPtCut(fileName):
   substr = fileName[fileName.find("_ptCut")+len("_ptCut"):dotOutPos]
   return substr
 
+def GetIasCut(filePath):
+  for line in open(filePath):
+    if "Ias" in line:
+      break
+  return line[line.find("(")+1:len(line)-2]
+
+def GetBackExp(filePath):
+  for line in open(BaseDir+file):
+    if "found" in line:
+      break
+  lineSplit = line.split(' ')
+  return float(lineSplit[1])
+
+def GetBackExpError(filePath):
+  for line in open(BaseDir+file):
+    if "found" in line:
+      break
+  lineSplit = line.split(' ')
+  return float(lineSplit[3])
+
+def GetSigEff(filePath):
+  for line in open(BaseDir+file):
+    if "event level" in line:
+      break
+  lineSplit = line.split(' ')
+  return float(lineSplit[0])
+
+
+
 fileList = os.listdir(BaseDir)
 
 print
-print 'Model         PtCut   IasCut   MassCut     BackExpOverIas'
+print 'Model         PtCut   IasCut     MassCut    BackExpOverIas    SignalEff'
 print
 
 for file in fileList:
   if ".out" in file:
-    for line in open(BaseDir+file):
-      if "found" in line:
-        break
-    lineSplit = line.split(' ')
-    backExp = float(lineSplit[1])
-    backExpError = float(lineSplit[3])
-
-    for line in open(BaseDir+file):
-      if "Ias" in line:
-        break
-    lineSplit = line[line.find("(")+1:len(line)-2]
-    iasCut  = lineSplit
+    backExp = GetBackExp(BaseDir+file)
+    backExpError = GetBackExpError(BaseDir+file)
+    iasCut  = GetIasCut(BaseDir+file)
+    sigEff = GetSigEff(BaseDir+file)
     massCut = GetMassCut(file)
     ptCut = GetPtCut(file)
     signalName = GetModelName(file)
-    printString = string.ljust(signalName,12)+string.center(ptCut,8)+string.center(iasCut,10)
-    printString+=string.center(massCut,10)
-    printString+=string.center(str(backExp)+' +/- '+str(backExpError),10)
+    printString = string.ljust(signalName,15)+string.ljust(ptCut,8)+string.ljust(iasCut,10)
+    printString+=string.center(massCut,8)
+    printString+=string.center(str(round(backExp,4))+' +/- '+str(round(backExpError,4)),20)
+    printString+=string.center(str(round(sigEff,4)),10)
     print printString
 
 
