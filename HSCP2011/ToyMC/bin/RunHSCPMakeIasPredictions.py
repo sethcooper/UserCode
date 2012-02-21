8ca82583959354c03fcb36f22a159981b4cb4423
scooper <>
2012-02-21 00:48:06 +0000

    Moving to new system for signal models.

--- HSCP2011/ToyMC/bin/RunHSCPMakeIasPredictions.py
+++ HSCP2011/ToyMC/bin/RunHSCPMakeIasPredictions.py
@@ -6,46 +6,43 @@ import os
 import sys
 import HSCPMakeIasPredictionsLaunchOnCondor
 import glob
+from SignalDefinitions import *
 from subprocess import call
 
 
-Date = "feb11"
+Date = "feb20"
 BaseCfg = "makeIasPredictions_template_cfg.py"
-InputRootFile = os.getcwd()+'/FARM_MakeHSCParticlePlots_dataWithTightRPCTrig_absEta_ptErrorPresel_feb6/outputs/makeHSCParticlePlots_feb1_Data2011_all.root'
-#InputRootFile = os.getcwd()+'/FARM_MakeHSCParticlePlots_feb1/outputs/makeHSCParticlePlots_feb1_Data2011_all.root'
-#DataDirBase = "/local/cms/user/cooper/data/hscp/428/makeHSCParticlePlotsOutput"
-#InputRootFile = DataDirBase+"/MakeHSCParticlePlots_2011data_jan13/outputs/makeHSCParticlePlots_jan13_Data2011.root"
-# MassCuts from EPS AN note
-#MassCuts = [30,50,80,110,120,150,180,210,220,220,230,240,250,270,280,290,300,330,340,350,400,410,440]
-# PtCuts from EPS AN note
-#PtCuts =   [50,50,40,40, 40, 50, 120,60, 50, 60, 60, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40]
+InputDataRootFile = os.getcwd()+'/FARM_MakeHSCParticlePlots_dataWithTightRPCTrig_absEta_ptErrorPresel_feb6/outputs/makeHSCParticlePlots_feb1_Data2011_all.root'
 
 # From final 2011 analysis
-# model/mass  Gluino600f10,Gluino1000f10,GMStau100,GMStau200,GMStau308,GMStau432,Stop200,Stop600
-#MassCuts =   [360,         540,          20,       120,      190,      260,      130]#,    360]
-#PtCuts   =   [50,          120,          65,       70,       70,       55,       60]#,     50]
-#IasCuts  =   [0.325,       0.225,        0.400,    0.400,    0.400,    0.375,    0.400]#,  0.325]
 # model/mass  Gluino1000f10,Gluino1200f10,Stop600,Stop800,GMStau494, Gluino1100f10
 #PtCuts   =   [120,          155,          50,     50,     65,        120]
 #IasCuts  =   [0.225,        0.200,        0.325,  0.275,  0.35,      0.225]
 #MassCuts =   [540,          600,          360,    450,    300,       570]
 # model/mass  Gluino1000f10,Gluino1200f10,Stop600,Stop800,GMStau494, Gluino1100f10
-PtCuts   =   [50,          50,          50,     50,     50,        50]
-IasCuts  =   [0.225,        0.200,        0.325,  0.275,  0.35,      0.225]
-MassCuts =   [540,          600,          360,    450,    300,       570]
+#PtCuts   =   [50,          50,          50,     50,     50,        50]
 
-# model/mass  Gluino600f10,Gluino1000f10,GMStau100,GMStau200,GMStau308,GMStau432,Stop200,Gluino1100f10,Gluino1200f10
-#PtCuts =      [50,          50,           50,       50,       50,       50,       50,    50,           50]
-#IasCuts =     [0.1,         0.1,          0.1,      0.1,      0.1,      0.1,      0.1,   0.1,          0.1]
-#MassCuts =    [360,         540,          20,       120,      190,      260,      130,   570,          600]
 
 JobName = "makeIasPredictions_allTracks_allEvents_ptIas_ptPresel_"
 JobName+=Date
 JobName+="_"
-FarmDirectory = "FARM_MakeIasPredictions_allTracks_allEvents_upperPcutC_ptPresel_allSlices_loosePtSB_"
+FarmDirectory = "FARM_MakeIasPredictions_moreModels_cutPt50GeV_"
 FarmDirectory+=Date
 
-HSCPMakeIasPredictionsLaunchOnCondor.SendCluster_Create(FarmDirectory,JobName,InputRootFile,
-                                            BaseCfg, MassCuts, PtCuts, IasCuts)
+modelList = []
+modelList.extend([Gluino300,Gluino400,Gluino500,Gluino600,Gluino700,Gluino800,Gluino900,Gluino1000,Gluino1100,Gluino1200])
+modelList.extend([GluinoN300,GluinoN400,GluinoN500,GluinoN600,GluinoN700,GluinoN800,GluinoN900,GluinoN1000,GluinoN1100,GluinoN1200])
+modelList.extend([Stop130,Stop200,Stop300,Stop400,Stop500,Stop600,Stop700,Stop800])
+modelList.extend([StopN130,StopN200,StopN300,StopN400,StopN500,StopN600,StopN700,StopN800])
+modelList.extend([GMStau100,GMStau126,GMStau156,GMStau200,GMStau247,GMStau308,GMStau370,GMStau432,GMStau494])
+modelList.extend([PPStau100,PPStau126,PPStau156,PPStau200,PPStau247,PPStau308])
+
+for model in modelList:
+  #HSCPMakeIasPredictionsLaunchOnCondor.SendCluster_Create(FarmDirectory,JobName,InputDataRootFile,
+  #                                            BaseCfg, model.massCut, model.ptCut, model.iasCut)
+  # Use Pt cut of 50 GeV
+  HSCPMakeIasPredictionsLaunchOnCondor.SendCluster_Create(FarmDirectory,JobName,InputDataRootFile,
+                                              BaseCfg, model.massCut, 50, model.iasCut)
+
 HSCPMakeIasPredictionsLaunchOnCondor.SendCluster_Submit()
 
