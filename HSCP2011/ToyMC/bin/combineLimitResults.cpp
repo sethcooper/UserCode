f6f248ee25da94873568c66abd5f814481632fcd
scooper <>
2012-03-09 12:28:54 +0000

    bugfix: look for signal name in file name.

--- HSCP2011/ToyMC/bin/combineLimitResults.cpp
+++ HSCP2011/ToyMC/bin/combineLimitResults.cpp
@@ -11,7 +11,7 @@
 using namespace std;
 using namespace RooStats;
 
-vector<string> getFilesInDir(string dir)
+vector<string> getFilesInDir(string dir, string signal)
 {
   vector<string> files;
   DIR *dp;
@@ -19,11 +19,14 @@ vector<string> getFilesInDir(string dir)
   if((dp  = opendir(dir.c_str())) == NULL)
   {
     cout << "Error(" << errno << ") opening " << dir << endl;
+    return vector<string>();
   }
 
   while ((dirp = readdir(dp)) != NULL) {
     string fileName = string(dirp->d_name);
     if(fileName.find("doLimits") == string::npos)
+      continue;
+    if(fileName.find(signal) == string::npos)
       continue;
     string toAdd = dir;
     toAdd+="/";
@@ -38,18 +41,20 @@ vector<string> getFilesInDir(string dir)
 int main()
 {
   //string outputDir = "FARM_makeScaledAndDoLimits_allTracks_allEvents_etaReweight_sameBinning_bugfix_ptErr_absEta_feb6/outputs/";
-  string outputDir = "FARM_makeScaledAndDoLimits_allTracks_allEvents_pt50Ias0p1_ih3_pt45_sameBins_ptPresel_absEta_feb8/outputs/";
+  //string outputDir = "FARM_makeScaledAndDoLimits_allTracks_allEvents_pt50Ias0p1_ih3_pt45_sameBins_ptPresel_absEta_feb8/outputs/";
+  //string outputDir = "FARM_makeScaledAndDoLimits_allTracks_allEvents_pt50Ias0p1_ih3_pt45_sameBins_ptPresel_absEta_feb15/outputs/";
+  string outputDir = "FARM_moreModels_cutPt50GeVIas0p1_includeTightRPC_Mar05/outputs/";
 
   vector<string> signals;
-  //signals.push_back("Gluino600");
-  //signals.push_back("Gluino1000");
-  //signals.push_back("Gluino1100");
-  //signals.push_back("Gluino1200");
+  signals.push_back("Gluino600");
+  signals.push_back("Gluino1000");
+  signals.push_back("Gluino1100");
+  signals.push_back("Gluino1200");
   signals.push_back("GMStau100");
-  //signals.push_back("GMStau200");
-  //signals.push_back("GMStau308");
-  //signals.push_back("GMStau432");
-  //signals.push_back("Stop200");
+  signals.push_back("GMStau200");
+  signals.push_back("GMStau308");
+  signals.push_back("GMStau432");
+  signals.push_back("Stop200");
   //signals.push_back("Stop600");
 
   for(vector<string>::const_iterator signalItr = signals.begin();
@@ -57,7 +62,8 @@ int main()
   {
     int fileCounter = 0;
     cout << "Doing " << *signalItr << endl;
-    vector<string> thisSignalFiles = getFilesInDir(outputDir+*signalItr);
+    //vector<string> thisSignalFiles = getFilesInDir(outputDir+*signalItr);
+    vector<string> thisSignalFiles = getFilesInDir(outputDir, *signalItr);
     HypoTestInverterResult* r = 0;
     HypoTestInverterResult* thisResult = 0;
     TFile* thisTFile = 0;
