0e674e8a05504431b59b31163a4e71db3dc795a5
scooper <>
2012-03-09 12:26:42 +0000

    Add bayesian option; fix signal name.

--- HSCP2011/ToyMC/bin/HSCPDoLimitsLaunchOnCondor.py
+++ HSCP2011/ToyMC/bin/HSCPDoLimitsLaunchOnCondor.py
@@ -27,6 +27,7 @@ Base_Cfg = ''
 Int_Lumi = 0
 Base_xml_channel = ''
 Base_xml_combined = ''
+Do_Bayesian = False
 
 
 def CreateTheShellFile(bgInputFile,sigInputFile,massCut,iasCut,ptCut,xSecMin,xSecMax,index):
@@ -34,35 +35,59 @@ def CreateTheShellFile(bgInputFile,sigInputFile,massCut,iasCut,ptCut,xSecMin,xSe
     global CopyRights
     global Jobs_Name
     global Base_macro
-    # variables for limit setting
-    nPoints = 8 # per job
-    nToys = 1000 # per xSec trial point
-    # end variables for limit setting
+    global Do_Bayesian
     posLastUndsc = sigInputFile.rfind("_")
-    endRemoved = sigInputFile[0:posLastUndsc]
-    posLastUndsc = endRemoved.rfind("_")
-    signalName = endRemoved[posLastUndsc+1:len(endRemoved)]
-    outputFile = 'doLimits_'+signalName+'_massCut'+`massCut`+'_ptCut'+`ptCut`+'_index'+str(index)+'.root'
-    Path_Shell = Farm_Directories[1]+Jobs_Name+signalName+'_massCut'+`massCut`+'_ptCut'+`ptCut`+'_index'+str(index)+'.sh'
-    shell_file=open(Path_Shell,'w')
-    shell_file.write('#!/bin/sh\n')
-    shell_file.write(CopyRights + '\n')
-    #shell_file.write('export SCRAM_ARCH=slc5_amd64_gcc434\n')
-    shell_file.write('# use later root\n')
-    #shell_file.write('source /afs/cern.ch/sw/lcg/external/gcc/4.3.2/x86_64-slc5/setup.sh\n')
-    shell_file.write('source /local/cms/user/cooper/root/bin/thisroot.sh\n')
-    shell_file.write('cd ' + os.getcwd() + '/' + Farm_Directories[4] + signalName + '\n')
-    shell_file.write('root -l -b -q "'+Base_macro
-        +'(\\"hscp_combined_hscp_model.root\\",\\"combined\\",\\"ModelConfig\\",\\"\\",\\"asimovData\\",0,3,true,'
-        +str(nPoints)+','
-        +str(xSecMin)+','
-        +str(xSecMax)+','
-        +str(nToys)+','
-        +'false,\\"\\",\\"'
-        +outputFile
-        +'\\")"'
-        + '\n')
-    #shell_file.write('mv ' + outputFile + ' ' + os.getcwd() + '/' + Farm_Directories[4] + '\n\n')
+    signalName = sigInputFile[posLastUndsc+1:len(sigInputFile)-5]
+    #endRemoved = sigInputFile[0:posLastUndsc]
+    #posLastUndsc = endRemoved.rfind("_")
+    #signalName = endRemoved[posLastUndsc+1:len(endRemoved)]
+    #print signalName
+    if(Do_Bayesian):
+      outputFile = 'doLimits_'+signalName+'_massCut'+`massCut`+'_ptCut'+`ptCut`+'_bayesian.root'
+      Path_Shell = Farm_Directories[1]+Jobs_Name+signalName+'_massCut'+`massCut`+'_ptCut'+`ptCut`+'_bayesian.sh'
+      shell_file=open(Path_Shell,'w')
+      shell_file.write('#!/bin/sh\n')
+      shell_file.write(CopyRights + '\n')
+      #shell_file.write('export SCRAM_ARCH=slc5_amd64_gcc434\n')
+      shell_file.write('# use later root\n')
+      #shell_file.write('source /afs/cern.ch/sw/lcg/external/gcc/4.3.2/x86_64-slc5/setup.sh\n')
+      shell_file.write('source /local/cms/user/cooper/root/bin/thisroot.sh\n')
+      shell_file.write('cd ' + os.getcwd() + '/' + Farm_Directories[0] + 'outputs/makeScaledPredictions/' + signalName + '\n')
+      shell_file.write('root -l -b -q "'+Base_macro
+        +'(\\"hscp_combined_hscp_model.root\\",\\"combined\\",\\"ModelConfig\\",\\"asimovData\\",\\"'+outputFile+'\\")"'+'\n')
+    else:
+      # CLs
+      # variables for limit setting
+      #nPoints = 8 # per job
+      nPoints = 4 # per job
+      #nToys = 1000 # per xSec trial point
+      nToys = 2000 # per xSec trial point
+      # end variables for limit setting
+      outputFile = 'doLimits_'+signalName+'_massCut'+`massCut`+'_ptCut'+`ptCut`+'_index'+str(index)+'.root'
+      Path_Shell = Farm_Directories[1]+Jobs_Name+signalName+'_massCut'+`massCut`+'_ptCut'+`ptCut`+'_index'+str(index)+'.sh'
+      shell_file=open(Path_Shell,'w')
+      shell_file.write('#!/bin/sh\n')
+      shell_file.write(CopyRights + '\n')
+      #shell_file.write('export SCRAM_ARCH=slc5_amd64_gcc434\n')
+      shell_file.write('# use later root\n')
+      #shell_file.write('source /afs/cern.ch/sw/lcg/external/gcc/4.3.2/x86_64-slc5/setup.sh\n')
+      shell_file.write('source /local/cms/user/cooper/root/bin/thisroot.sh\n')
+      shell_file.write('cd ' + os.getcwd() + '/' + Farm_Directories[0] + 'outputs/makeScaledPredictions/' + signalName + '\n')
+      shell_file.write('root -l -b -q "'+Base_macro
+          # Frequentist calculator
+          #+'(\\"hscp_combined_hscp_model.root\\",\\"combined\\",\\"ModelConfig\\",\\"\\",\\"asimovData\\",0,3,true,'
+          # Hybrid calculator
+          +'(\\"hscp_combined_hscp_model.root\\",\\"combined\\",\\"ModelConfig\\",\\"\\",\\"asimovData\\",1,3,true,'
+          #asymptCLs#+'(\\"hscp_combined_hscp_model.root\\",\\"combined\\",\\"ModelConfig\\",\\"\\",\\"asimovData\\",2,3,true,'
+          +str(nPoints)+','
+          +str(xSecMin)+','
+          +str(xSecMax)+','
+          +str(nToys)+','
+          +'false,\\"\\",\\"'
+          +outputFile
+          +'\\")"'
+          + '\n')
+    shell_file.write('mv ' + outputFile + ' ' + os.getcwd() + '/' + Farm_Directories[4] + '\n\n')
     shell_file.close()
     os.system("chmod 777 "+Path_Shell)
 
@@ -99,12 +124,19 @@ def AddJobToCmdFile(massCut,ptCut,sigInputFile,index):
     global Path_Shell
     global Path_Cmd
     global Jobs_Name
+    global Do_Bayesian
+    #posLastUndsc = sigInputFile.rfind("_")
+    #endRemoved = sigInputFile[0:posLastUndsc]
+    #posLastUndsc = endRemoved.rfind("_")
+    #signalName = endRemoved[posLastUndsc+1:len(endRemoved)]
     posLastUndsc = sigInputFile.rfind("_")
-    endRemoved = sigInputFile[0:posLastUndsc]
-    posLastUndsc = endRemoved.rfind("_")
-    signalName = endRemoved[posLastUndsc+1:len(endRemoved)]
-    Path_Log   = os.getcwd()+'/'+Farm_Directories[2]+Jobs_Name+signalName+'_massCut'+`massCut`+'_ptCut'+`ptCut`+'_index'+str(index)
-    Path_Error   = os.getcwd()+'/'+Farm_Directories[3]+Jobs_Name+signalName+'_massCut'+`massCut`+'_ptCut'+`ptCut`+'_index'+str(index)
+    signalName = sigInputFile[posLastUndsc+1:len(sigInputFile)-5]
+    if(Do_Bayesian):
+      Path_Log   = os.getcwd()+'/'+Farm_Directories[2]+Jobs_Name+signalName+'_massCut'+`massCut`+'_ptCut'+`ptCut`+'_bayesian'
+      Path_Error   = os.getcwd()+'/'+Farm_Directories[3]+Jobs_Name+signalName+'_massCut'+`massCut`+'_ptCut'+`ptCut`+'_bayesian'
+    else:
+      Path_Log   = os.getcwd()+'/'+Farm_Directories[2]+Jobs_Name+signalName+'_massCut'+`massCut`+'_ptCut'+`ptCut`+'_index'+str(index)
+      Path_Error   = os.getcwd()+'/'+Farm_Directories[3]+Jobs_Name+signalName+'_massCut'+`massCut`+'_ptCut'+`ptCut`+'_index'+str(index)
     cmd_file=open(Path_Cmd,'a')
     cmd_file.write('\n')
     cmd_file.write('Executable              = %s\n'     % Path_Shell)
@@ -115,36 +147,54 @@ def AddJobToCmdFile(massCut,ptCut,sigInputFile,index):
     #cmd_file.write(os.getcwd() + '/' + Path_Shell + '\n')
     cmd_file.close()
 
-def SendCluster_Create(FarmDirectory, jobName, intLumi, baseMacro):
+def CreateDirectoryStructure(FarmDirectory):
+    global Jobs_Name
+    global Farm_Directories
+    Farm_Directories = [FarmDirectory+'/', FarmDirectory+'/inputs/doLimits/',
+                        FarmDirectory+'/logs/doLimits/',
+                        FarmDirectory+'/errors/doLimits/',
+                        FarmDirectory+'/outputs/doLimits/']
+    for i in range(0,len(Farm_Directories)):
+        if os.path.isdir(Farm_Directories[i]) == False:
+            os.system('mkdir -p ' + Farm_Directories[i])
+
+def SendCluster_Create(FarmDirectory, jobName, intLumi, baseMacro, doBayesian):
     global Jobs_Name
     global Jobs_Count
     global Base_macro
     global Int_Lumi
     global Farm_Directories
+    global Do_Bayesian
     Jobs_Name  = jobName
     Jobs_Count = 0
     Base_macro = baseMacro
     Int_Lumi=intLumi
-    Farm_Directories = [FarmDirectory+'/', FarmDirectory+'/inputs/', FarmDirectory+'/logs/',
-                        FarmDirectory+'/errors/', FarmDirectory+'/outputs/']
+    Do_Bayesian=doBayesian
+    CreateDirectoryStructure(FarmDirectory)
     CreateTheCmdFile()
 
 
 def SendCluster_Push(bgInputFilesBase,sigInputFile,massCut,iasCut,ptCut):
     global Jobs_Count
     global Jobs_Index
+    global Do_Bayesian
     Jobs_Index = "%04i" % Jobs_Count
     bgInputFile = bgInputFilesBase+`massCut`+'_ptCut'+`ptCut`+'.root'
-    xSecMin = 0.0001
-    xSecMax = 0.05
-    nSteps = 100
-    stepSize = (xSecMax-xSecMin)/nSteps
-    for index in range(0,nSteps):
-      xSecMinPt = xSecMin+index*stepSize
-      xSecMaxPt = xSecMin+(index+1)*stepSize
-      CreateTheShellFile(bgInputFile,sigInputFile,massCut,iasCut,ptCut,xSecMinPt,xSecMaxPt,index)
-      AddJobToCmdFile(massCut,ptCut,sigInputFile,index)
+    if(Do_Bayesian):
+      CreateTheShellFile(bgInputFile,sigInputFile,massCut,iasCut,ptCut,0,0,0)
+      AddJobToCmdFile(massCut,ptCut,sigInputFile,0)
       Jobs_Count = Jobs_Count+1
+    else:
+      xSecMin = 0.0001
+      xSecMax = 0.025
+      nSteps = 100
+      stepSize = (xSecMax-xSecMin)/nSteps
+      for index in range(0,nSteps):
+        xSecMinPt = xSecMin+index*stepSize
+        xSecMaxPt = xSecMin+(index+1)*stepSize
+        CreateTheShellFile(bgInputFile,sigInputFile,massCut,iasCut,ptCut,xSecMinPt,xSecMaxPt,index)
+        AddJobToCmdFile(massCut,ptCut,sigInputFile,index)
+        Jobs_Count = Jobs_Count+1
 
 def SendCluster_Submit():
     global CopyRights
